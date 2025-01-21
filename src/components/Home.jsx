db0f500bdc2529f32540743bafe05102f42fa9ac
Kavindu Maleesha <91423119+Kavimaleesha@users.noreply.github.com>
2025-01-21 13:19:22 +0530

    Update zip file data

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {AiFillLinkedin,AiFillGithub} from 'react-icons/ai'
+import kmpic from './sampleData/kavinduimage.png'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='flex mx-auto items-center justify-center mt-20'>
+        <img src={kmpic} alt="" className='w-80 h-80 size-fit bg-gradient-to-b from-yellow-100 rounded-full' />
+      </div>
+      <div className='text-center p-10'>
+        <h2 className='text-5xl py-2 text-yellow-600 font-medium'>Kavindu Maleesha</h2>
+        <h3 className='text-2xl py-2 text-white'>Software Engineer</h3>
+        <div className='flex justify-center'>
+          <p className='text-md py-5 leading-5 text-gray-400 md:text-lg md:w-[800px] '>
+          Hello there! I am a Bachelor of Software Engineering undergraduate at Open University seeking opportunities to enhance my knowledge by working in teams. 
+          Therefore, I'm looking for a challenging career in an organization where I can contribute my skills and competencies. Also, 
+          I'm waiting to improve my abilities and gain experience with a new career in my life.
+          </p>
+        </div>
+      </div>
+      <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+        <a href='https://www.linkedin.com/in/kavindu-maleesha-93b952219/' target='blank'><AiFillLinkedin /></a>
+        <a href='https://github.com/Kavimaleesha' target='blank'><AiFillGithub /></a>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/sampleData/moduleList.js
@@ -0,0 +1,243 @@
+
+export const moduleList = [
+    {
+        id:1,
+        moduleName:'Communication Skills',
+    },
+    {
+        id:2,
+        moduleName:'Economics and Marketing for Engineering',
+    },
+    {
+        id:3,
+        moduleName:'Computer Security Concepts',
+    },
+    {
+        id:4,
+        moduleName:'Introduction to object oriented programming',
+    },
+    {
+        id:5,
+        moduleName:'Information systems and data management',
+    },
+    {
+        id:6,
+        moduleName:'Web application development',
+    },
+    {
+        id:7,
+        moduleName:'Programming in Python',
+    },
+    {
+        id:8,
+        moduleName:'Requirement engineering',
+    },
+    {
+        id:9,
+        moduleName:'Web technology',
+    },
+    {
+        id:10,
+        moduleName:'User experience engineering',
+    },
+    {
+        id:11,
+        moduleName:'Object oriented design',
+    },
+    {
+        id:12,
+        moduleName:'Data modelling and database systems',
+    },
+    {
+        id:13,
+        moduleName:'Software Testing and Quality Assurance',
+    },
+    {
+        id:14,
+        moduleName:'Communication and computer technology',
+    },
+    {
+        id:15,
+        moduleName:'Software Engineering Concepts and Programming',
+    },
+    {
+        id:16,
+        moduleName:'Data structures and algorithms',
+    },
+    {
+        id:17,
+        moduleName:'Software design in group',
+    },
+    {
+        id:18,
+        moduleName:'Fundamentals of Information Systems',
+    },
+    {
+        id:19,
+        moduleName:'Introduction to Laws of Sri Lanka',
+    },
+    {
+        id:20,
+        moduleName:'Basic mathematics for computing',
+    },
+    {
+        id:21,
+        moduleName:'Mathematics for Computing',
+    },
+]
+
+
+export const projectList = [
+    {
+        id:1,
+        projectName:"Hirely AI Web Site",
+        projectDiscription:" AI Powered Hiring Platform",
+        ProjectLink_id1:"Hirely AI" ,
+        projectTools:[
+            {
+                tool:"React JS"
+            },
+            {
+                tool:"Node JS"
+            },
+            {
+                tool:"Mongo DB"
+            },
+            {
+                tool:"Tailwind CSS"
+            },
+            {
+                tool:"Expess Js"
+            },
+        ]
+    },
+    {
+        id:2,
+        projectName:"User Management System ",
+        projectDiscription:"User Management System using MERN Stack CRUD Application..",
+        projectTools:[
+            {
+                tool:"React JS"
+            },
+            {
+                tool:"Node JS"
+            },
+            {
+                tool:"Mongo DB"
+            },
+            {
+                tool:"Expess Js"
+            },
+        ]
+    },
+    {
+        id:3,
+        projectName:"FitClub - Gym Website",
+        projectDiscription:"Website for Gym.",
+        projectTools:[
+            {
+                tool:"Ract Js"
+            },
+            {
+                tool:"CSS"
+            },
+
+
+        ]
+    },
+    {
+        id:4,
+        projectName:"Clinical Care",
+        projectDiscription:"Website for Patients to Reserve Clinical Time Slots. (Group project)",
+        projectTools:[
+            {
+                tool:"PHP"
+            },
+            {
+                tool:"SQL"
+            },
+            {
+                tool:"HTML"
+            },
+            {
+                tool:"JavaScript"
+            },
+            {
+                tool:"CSS"
+            },
+
+        ]
+    },
+    {
+        id:5,
+        projectName:"eBook App",
+        projectDiscription:"ebook app for library using react native where user can view book information.",
+        projectTools:[
+            {
+                tool:"Ract Native"
+            },
+            {
+                tool:"CSS"
+            },
+
+        ]
+    },
+    {
+        id:6,
+        projectName:"Tourism Website about Sri Lanka",
+        projectDiscription:"Web Site for display details about Sri Lanka.",
+        projectTools:[
+            {
+                tool:"HTML"
+            },
+            {
+                tool:"CSS"
+            },
+            {
+                tool:"JavaScript"
+            },
+        ]
+    },
+    {
+        id:7,
+        projectName:"Cab Service System",
+        projectDiscription:"Calculate price of cab serice based on vehicle type, number of passengers and payload.",
+        projectTools:[
+            {
+                tool:"Python"
+            },
+
+        ]
+    },
+    {
+        id:8,
+        projectName:"Crud Forum",
+        projectDiscription:"Web application perform crud opperations using php. ",
+        projectTools:[
+            {
+                tool:"PHP"
+            },
+            {
+                tool:"HTML"
+            },
+            {
+                tool:"CSS"
+            },
+        ]
+    },
+    {
+        id:9,
+        projectName:"Portfolio",
+        projectDiscription:"My Portfolio - Kavindu Maleesha",
+        projectTools:[
+            {
+                tool:"React JS"
+            },
+            {
+                tool:"CSS"
+            },
+        ]
+    },
+
+]
+
